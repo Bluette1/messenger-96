2271e54d129861db3650ada11502b6bdc154b5cc
Bluette1 <marylene.sawyer@gmail.com>
2021-06-24 15:58:49 +0200

    Initial set up
    - Install dependencies
    - Establish database connection

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,18 @@
 const Sequelize = require("sequelize");
+const db = new Sequelize(process.env.DATABASE_URL || "postgresql://mary:password@localhost:5432/messenger", {
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
   logging: false
 });
+
+async function tryConnection() {
+  try {
+    await db.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+tryConnection();
 
 module.exports = db;
